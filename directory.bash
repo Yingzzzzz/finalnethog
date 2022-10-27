763ac8260360a462624c1fe10d682f2548941446
Yingzzzzz <96532087+Yingzzzzz@users.noreply.github.com>
2022-10-27 09:51:48 +0300

    Add files via upload

---
+++ directory.bash
@@ -0,0 +1,15 @@
+#mkdir web_monitor_log
+#cd web_monitor_log
+#touch Web_Log.csv  #Final output
+
+#Time
+
+#Monitor command
+nethogs -t -d 100 -c 5 >> WebLog.csv 
+
+#duplicate
+#awk -F '|' '!x[$1]++' WebLog.csv > Web_Log.csv
+#output
+#sed -i 's/\s\+/,/g' Web_Log.csv
+#awk -F '|' '!x[$1]++' Web_Log.csv
+
