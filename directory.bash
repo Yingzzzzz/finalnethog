beefcff526d25aa49cfcb427938f1a69fd031c93
Yingzzzzz <96532087+Yingzzzzz@users.noreply.github.com>
2022-10-27 09:53:15 +0300

    Update directory.bash

--- directory.bash
+++ directory.bash
@@ -5,7 +5,8 @@
 #Time
 
 #Monitor command
-nethogs -t -d 100 -c 5 >> WebLog.csv 
+#nethogs -t -d 100 -c 5 >> WebLog.csv 
+nethogs -v1 -t -c 5  >> WebLog.csv 
 
 #duplicate
 #awk -F '|' '!x[$1]++' WebLog.csv > Web_Log.csv
